f7e7f827f6145b202d238d03f6f30f983e599a10
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-08 18:28:41 -0300

    create api

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,32 +1,36 @@
 from django.urls import path
-from recipes.views import site
-
+from recipes import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', site.RecipeListViewHome.as_view(), name="home"),
+    path('', views.RecipeListViewHome.as_view(), name="home"),
     path(
         'recipes/search/',
-        site.RecipeListViewSearch.as_view(),
+        views.RecipeListViewSearch.as_view(),
         name="search"
     ),
     path(
         'recipes/tags/<slug:slug>/',
-        site.RecipeListViewTag.as_view(),
+        views.RecipeListViewTag.as_view(),
         name="tag"
     ),
     path('recipes/category/<int:category_id>/',
-         site.RecipeListViewCategory.as_view(), name="category"),
-    path('recipe/<int:pk>/', site.RecipeDetail.as_view(), name="recipe"),
+         views.RecipeListViewCategory.as_view(), name="category"),
+    path('recipe/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
     path(
         'recipes/api/v1/',
-        site.RecipeListViewHomeApi.as_view(),
+        views.RecipeListViewHomeApi.as_view(),
         name="recipe_api_v1"
     ),
     path(
         'recipes/api/v1/<int:pk>/',
-        site.RecipeDetailAPI.as_view(),
+        views.RecipeDetailAPI.as_view(),
         name="recipe_api_v1_detail",
     ),
-    path('recipes/theory/', site.theory, name="theory")
+    path('recipes/theory/', views.theory, name="theory"),
+    path(
+        'recipes/api/v2/', 
+        views.recipe_api_list,
+        name='recipe_api_v2',
+    ),
 ]

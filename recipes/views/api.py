38b054c86e1715e81e82a21ad994f8a7d036ddc8
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-08 23:16:53 -0300

    update

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,10 +3,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from recipes.models import Recipe
+from recipes.serializers import RecipeSerializer
 
 
 @api_view()
 def recipe_api_list(request):
-    return Response({
-        "name": 'blablalba'
-    })
+    recipes = Recipe.objects.get_published()[:10]
+    serializers = RecipeSerializer(instance=recipes)
+    return Response(serializers.data)

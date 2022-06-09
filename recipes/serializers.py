0a1dc41bfdbed3726447c2773c3ab24775f0cf02
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-09 01:47:25 -0300

    update

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,40 +1,44 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from tag.models import Tag
+from .models import Recipe
 
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    slug = serializers.SlugField(max_length=255)
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
 
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author',
+            'category', 'tags', 'public', 'preparation',
+            'tag_objects', 'tag_links'
+        ]
+
+    public = serializers.BooleanField(
+        source='is_published',
+        read_only=True,
+    )
     preparation = serializers.SerializerMethodField(
-        method_name='any_method_name'
+        method_name='any_method_name',
+        read_only=True,
     )
 
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
+    category = serializers.StringRelatedField(
+        read_only=True,
     )
 
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True
-    )
     tag_objects = TagSerializer(
         many=True, source='tags'
     )
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',
-        queryset=Tag.objects.all(),
-        view_name='recipes:recipe_api_v2_tag'
+        view_name='recipes:recipe_api_v2_tag',
+        read_only=True,
     )
 
     def any_method_name(self, recipe):

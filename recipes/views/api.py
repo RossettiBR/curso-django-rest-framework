de99971692c6394bf59abe8dfb79f729d3e6a2f9
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-11 01:26:55 -0300

    refactor code

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -5,8 +5,7 @@ from recipes.serializers import RecipeSerializer, TagSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tag.models import Tag
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -14,16 +13,27 @@ class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
 
-class RecipeAPIV2List(ListCreateAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
-
-class RecipeAPIV2Detail(RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
+    def partial_update(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        
+        recipe = self.get_queryset().filter(pk=pk).first()
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+        )
 
 
 @api_view()

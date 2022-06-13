29e3069c3b3cea7f793baf589a3028f43ff5c1b4
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-13 00:55:20 -0300

    update, create urls JWT

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
 from recipes import views
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = 'recipes'
 
@@ -42,6 +47,21 @@ urlpatterns = [
         'recipes/api/v2/tag/<int:pk>/',
         views.tag_api_detail,
         name='recipe_api_v2_tag',
+    ),
+    path(
+        'recipes/api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'recipes/api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'recipes/api/token/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'
     ),
     path('', include(recipe_api_v2_router.urls))
 ]

cbcc8584d1d3b3446dfd58caf0ee8a839ab6d511
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-13 01:29:39 -0300

    create permission_classes

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from tag.models import Tag
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
@@ -17,6 +18,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticated, ]
 
     def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')

75e7eef25c0a0ec2d38c7958de4287f300180966
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-14 00:20:31 -0300

    Update permissions

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import get_object_or_404
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer, TagSerializer
@@ -7,7 +6,8 @@ from rest_framework.response import Response
 from tag.models import Tag
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from ..permissions import IsOwner
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
@@ -18,12 +18,28 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return[IsOwner(), ]
+
+        return super().get_permissions()
 
     def partial_update(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-
-        recipe = self.get_queryset().filter(pk=pk).first()
+        recipe = self.get_object()
         serializer = RecipeSerializer(
             instance=recipe,
             data=request.data,
